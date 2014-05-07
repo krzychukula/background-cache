2a881f0e1f40cdfade8560862e3b11fae4530ee9
Jakub Wasilewski <jakub.wasilewski@schibsted.pl>
2014-05-07 15:41:55 +0200

    Cache now checks for expiry on retrieval instead of using background timers.

--- cache.js
+++ cache.js
@@ -7,6 +7,8 @@ var cacheStore = {
 
 }
 
+var defaultTimeToLive = 40 * 1000; // 40sec
+
 //when application starts get contents of cache from redis
 client.hgetall("cache", function (err, obj) {
     if(err){
@@ -19,41 +21,37 @@ client.hgetall("cache", function (err, obj) {
             // prop is not inherited
             console.log('loading key from redis');
             cacheStore[key] = {
-                responseBody: JSON.parse(obj[key])
+                cached: JSON.parse(obj[key])
             }
         }
    }
 });
 
+function now() {
+    return (new Date()).getTime();
+}
+
 function parseJson(data){
     return JSON.parse(data);
 }
 
-exports.resource = function(requestOptions, handler, time){
-    time = time || (1000 * 40);//1000 = 1s
+exports.resource = function(requestOptions, handler, timeToLive){
+    timeToLive = timeToLive || defaultTimeToLive;
     handler = handler || parseJson;
     var keyPart = requestOptions.url ? requestOptions.url : requestOptions;
     var key = JSON.stringify(keyPart);
 
-    var existing = cacheStore[key];
-    if(existing && existing.promised){
-        return existing.promised;
-    }else if(existing){
-        //just go and set update function but do not reset cacheStore[key]
-    } else {
-        cacheStore[key] = {
-            responseBody: ''
-        };
-    }
-
-    function update(){
+    function updateCachedEntry(){
         request.get(requestOptions, function (err, response, responseBody) {
             if (!err && response.statusCode == 200) {
                 try{
                     // cache the body of the response
-                    cacheStore[key].responseBody = responseBody;
+                    cacheStore[key].cached = {
+                        responseBody: responseBody,
+                        expires: now() + timeToLive
+                    };
                     if(client.connected){
-                        client.hmset("cache", key, JSON.stringify(cacheStore[key].responseBody));
+                        client.hmset("cache", key, JSON.stringify(cacheStore[key].cached));
                     }
                 }catch(e){
                     console.error("Can't update CACHE: ", key, " due to", e);
@@ -62,21 +60,34 @@ exports.resource = function(requestOptions, handler, time){
                 console.error('error: cache ', JSON.stringify(requestOptions), err);
             }
         });
-        setTimeout(update, time);
     }
 
-    process.nextTick(update);
+    var existing = cacheStore[key];
+    if(existing && existing.promised){
+        return existing.promised;
+    }else if(existing){
+        //just go and set update function but do not reset cacheStore[key]
+    } else {
+        cacheStore[key] = {};
+        updateCachedEntry(); // implicit update to get the first cached value
+    }
 
     cacheStore[key].promised = {
         get: function(){
-            if (cacheStore[key].responseBody) {
-                return handler(cacheStore[key].responseBody);
+            var entry = cacheStore[key];
+            if (entry.cached) {
+                var timeToRefresh = now() >= entry.cached.expires;
+                if (timeToRefresh) {
+                    updateCachedEntry();
+                }
+
+                return handler(entry.cached.responseBody);
             } else {
                 return '';
             }
         }
-    }
+    };
 
     return cacheStore[key].promised;
 
-}
+};
