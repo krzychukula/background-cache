22f075fee8b0fad60d85a4fb76dc61ad83ef8803
Jakub Wasilewski <jakub.wasilewski@schibsted.pl>
2014-05-07 14:41:24 +0200

    Cache now stores the response body (not parsed output), and the key is simplified to just the request URL.

--- cache.js
+++ cache.js
@@ -19,7 +19,7 @@ client.hgetall("cache", function (err, obj) {
             // prop is not inherited
             console.log('loading key from redis');
             cacheStore[key] = {
-                parsed: JSON.parse(obj[key])
+                responseBody: JSON.parse(obj[key])
             }
         }
    }
@@ -33,28 +33,27 @@ exports.resource = function(requestOptions, handler, time){
     time = time || (1000 * 40);//1000 = 1s
     handler = handler || parseJson;
     var keyPart = requestOptions.url ? requestOptions.url : requestOptions;
-    var key = JSON.stringify(keyPart)+handler+time;
+    var key = JSON.stringify(keyPart);
 
-    var existing = cacheStore[key]
+    var existing = cacheStore[key];
     if(existing && existing.promised){
         return existing.promised;
-    }else if(existing && !existing.promise){
+    }else if(existing){
         //just go and set update function but do not reset cacheStore[key]
     } else {
         cacheStore[key] = {
-            parsed: ''
+            responseBody: ''
         };
     }
-
 
     function update(){
         request.get(requestOptions, function (err, response, responseBody) {
             if (!err && response.statusCode == 200) {
                 try{
-                    var parsed = handler(responseBody);
-                    cacheStore[key].parsed = parsed;
+                    // cache the body of the response
+                    cacheStore[key].responseBody = responseBody;
                     if(client.connected){
-                        client.hmset("cache", key, JSON.stringify(cacheStore[key].parsed));
+                        client.hmset("cache", key, JSON.stringify(cacheStore[key].responseBody));
                     }
                 }catch(e){
                     console.error("Can't update CACHE: ", key, " due to", e);
@@ -70,7 +69,11 @@ exports.resource = function(requestOptions, handler, time){
 
     cacheStore[key].promised = {
         get: function(){
-            return cacheStore[key].parsed;
+            if (cacheStore[key].responseBody) {
+                return handler(cacheStore[key].responseBody);
+            } else {
+                return '';
+            }
         }
     }
 
